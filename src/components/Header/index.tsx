d392880b61b4760850c7b20826ef004cef9cda4a
martoio <martoio@yahoo.com>
2020-10-06 19:40:58 +0300

    Add Header navigation buttons (#9)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,8 @@ import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 
 export interface HeaderProps {
     onClickHome: () => void;
+    goToNextRecord: () => void;
+    goToPreviousRecord: () => void;
 }
 
 export const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
@@ -12,7 +14,9 @@ export const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                <Nav.Link onClick={props.onClickHome}>Home</Nav.Link>
+                    <Nav.Link onClick={props.onClickHome}>Home</Nav.Link>
+                    <Nav.Link onClick={props.goToPreviousRecord}>&lt;&lt; Back</Nav.Link>
+                    <Nav.Link onClick={props.goToNextRecord}>Next &gt;&gt;</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
