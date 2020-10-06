4a1bfe006ce7737a713d56e38227ee2c3b9875cc
martoio <martoio@yahoo.com>
2020-10-06 17:37:03 +0300

    [PR-07] Add lunr (#7)
    * Add lunr to FilterPane and bind to filtering the records
    
    * Fix initial state on app load

--- src/components/FilterPane/index.tsx
+++ src/components/FilterPane/index.tsx
@@ -4,8 +4,23 @@ import {FilterPaneWrapper} from './styles';
 import { Form } from 'react-bootstrap';
 
 export interface FilterPaneProps {
-
+    onNegateUpdate: (terms: string) => void;
+    onSearchUpdate: (terms: string) => void;
 }
+
+export const updateNegateFilter = (value: string, onNegateUpdate: (terms: string) => void): void => {
+    if(value.length === 0) {
+        onNegateUpdate('');
+        return;
+    }
+    const words = value.trim().split(' ').map(word => `-${word}`).join(' ');
+    onNegateUpdate(words);
+};
+
+export const updateSearchFilter = (value: string, onSearchUpdate: (terms: string) => void) => {
+    const words = value.trim();
+    onSearchUpdate(words);
+};
 
 export const FilterPane: React.FC<FilterPaneProps> = (props: FilterPaneProps) => {
     return (
@@ -13,13 +28,21 @@ export const FilterPane: React.FC<FilterPaneProps> = (props: FilterPaneProps) =>
             FilterPane
             <Form>
                 <Form.Group controlId='shouldBooleanQuery'>
-                    <Form.Control placeholder=""/>
+                    <Form.Control
+                        placeholder="nobel prize"
+                        onKeyUp={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                            updateSearchFilter(e.currentTarget.value, props.onSearchUpdate);
+                        }} />
                     <Form.Text>
                         Space separated list of words to search for.
                     </Form.Text>
                 </Form.Group>
                 <Form.Group controlId='notBooleanQuery'>
-                    <Form.Control ></Form.Control>
+                    <Form.Control
+                        placeholder="journal medicine"
+                        onKeyUp={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                            updateSearchFilter(e.currentTarget.value, props.onNegateUpdate);
+                        }}></Form.Control>
                     <Form.Text>
                         Space separated list of words to exclude.
                     </Form.Text>
---
+++ src/searchIndex.ts
@@ -0,0 +1,19 @@
+import lunr from 'lunr';
+import Record from './models/record';
+
+const IndexBuilder = new lunr.Builder();
+IndexBuilder.ref('id');
+IndexBuilder.field('id');
+IndexBuilder.field('title');
+IndexBuilder.field('body');
+
+export const populateIndex = (records: Array<Record>) => {
+    records.forEach(record => {
+        IndexBuilder.add(record);
+    });
+};
+
+export const generateIndex = (records: Array<Record>) => {
+    populateIndex(records);
+    return IndexBuilder.build();
+};
